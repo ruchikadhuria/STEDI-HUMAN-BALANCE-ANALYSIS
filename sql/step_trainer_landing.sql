2ecb3d6d7898c60a82e19a6e110c63105c241e8a
ruchikadhuria <133236060+ruchikadhuria@users.noreply.github.com>
2026-07-07 18:26:25 +0530

    Update step_trainer_landing.sql

--- sql/step_trainer_landing.sql
+++ sql/step_trainer_landing.sql
@@ -1,1 +1,7 @@
-
+CREATE EXTERNAL TABLE IF NOT EXISTS stedi.step_trainer_landing (
+ sensorreadingtime bigint,
+ serialnumber string,
+ distancefromobject double
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://stedi-data-lake-ruchika/step_trainer/landing/';
